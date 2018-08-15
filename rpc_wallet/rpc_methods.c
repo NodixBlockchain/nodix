725e284fa29c3cbd3c0dbfda6d5c95705422422b
purenode <purenode@social-souls.com>
2018-08-15 18:01:59 +0200

    2010 fix

--- rpc_wallet/rpc_methods.c
+++ rpc_wallet/rpc_methods.c
@@ -4071,25 +4071,6 @@ OS_API_C_FUNC(int) getpubaddrs(mem_zone_ref_const_ptr params, unsigned int rpc_m
 	return 1;
 }
 
-inline float fast_log(float val)
-{
-	int * const exp_ptr = (int *)(&val);
-	int x = *exp_ptr;
-	const int log_2 = ((x >> 23) & 255) - 128;
-	x &= ~(255 << 23);
-	x += 127 << 23;
-	*exp_ptr = x;
-
-	val = ((-1.0f / 3) * val + 2) * val - 2.0f / 3;
-	return ((val + log_2) * 0.69314718f);
-}
-
-float getdifficulty(unsigned int bits)
-{
-	double max_body = fast_log(0x00ffff), scaland = fast_log(256);
-	return exp(max_body - fast_log(bits & 0x00ffffff) + scaland * (0x1d - ((bits & 0xff000000) >> 24)));
-}
-
 
 OS_API_C_FUNC(int) getmininginfo(mem_zone_ref_const_ptr params, unsigned int rpc_mode, mem_zone_ref_ptr result)
 {
